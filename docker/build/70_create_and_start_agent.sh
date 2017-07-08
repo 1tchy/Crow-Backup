15d66c91ef2fabdff73a329c32051a9870e5f177
Itchy <itchy@laurinmurer.ch>
2017-07-08 22:14:50 +0200

    #23: Brought TestFX-Test to work also on Teamcity

--- docker/build/70_create_and_start_agent.sh
+++ docker/build/70_create_and_start_agent.sh
@@ -6,4 +6,7 @@ docker create --name=teamcity-agent --privileged -e TEAMCITY_SERVER=$SERVER sjoe
 
 docker start teamcity-agent
 
+docker exec -it teamcity-agent apt-get update
+docker exec -it teamcity-agent apt-get install -y libgtk2.0-0 libxtst6 libxxf86vm1 libglu1-mesa
+
 open $SERVER/agents.html?tab=unauthorizedAgents
